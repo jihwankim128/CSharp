6c2809ac228fa712e8f9eb9084dc7f4d77e8ced1
jihwan <jihwan1535@nate.com>
2023-08-03 15:09:37 +0900

    Declaration And Print Of Real Number

---
+++ 013_Data6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013_Data6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 3.1415926535897932384626433832f;
+            double b = 3.1415926535897932384626433832;
+            decimal c = 3.1415926535897932384626433832m;
+
+            Console.WriteLine("float = " + a);
+            Console.WriteLine("double = " + b);
+            Console.WriteLine("decimal = " + c);
+        }
+    }
+}
