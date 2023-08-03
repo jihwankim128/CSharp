127824d001b8a7e63ba5e1ecbaebd6b553d749b9
jihwan <jihwan1535@nate.com>
2023-08-03 15:05:03 +0900

    Data Type Casting

---
+++ 010_Data3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010_Data3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte sbyteData = 255;
+            // Over Casting
+            sbyte sbyteData2 = (sbyte)sbyteData;
+            // Auto Casting
+            int intData = sbyteData;
+
+            Console.WriteLine("sbyteData = " + sbyteData);
+            Console.WriteLine("sbyteData2 = " + sbyteData2);
+            Console.WriteLine("intData = " + intData);
+        }
+    }
+}
