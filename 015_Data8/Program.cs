d2b2331f7e08ab8ae2489842f706d5e9e15148b7
jihwan <jihwan1535@nate.com>
2023-08-03 15:10:27 +0900

    Char <-> Int Casting

---
+++ 015_Data8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015_Data8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = 'A';
+            // upCasting - auto
+            int num = a;
+
+            Console.WriteLine("문자 : {0}, 아스키 코드 값: {1}, 문자로 다운 캐스팅: {2}", a, num, (char)num);
+        }
+    }
+}
