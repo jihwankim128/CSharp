e15c7f697586920d773d266d372aa753f78c8405
jihwan <jihwan1535@nate.com>
2023-08-03 14:58:12 +0900

    Basic Test

---
+++ 005_Check/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("지금 듣는 강의는 ");
+            for(int i = 0; i< args.Length; i++)
+            {
+                Console.Write(args[i] + " ");
+            }
+            Console.WriteLine("좋아요!!");
+        }
+    }
+}
