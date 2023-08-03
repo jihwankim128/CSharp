8d1f39fc9045287cb30244af65d2d3d5660585d3
jihwan <jihwan1535@nate.com>
2023-08-03 15:00:32 +0900

    Declaring various Int

---
+++ 007_Variable2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_Variable2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100;
+            int y = 200;
+            int a = 1000, b = 2000;
+            Console.WriteLine("x : {0}, y : {1}", x, y);
+            Console.WriteLine("a : {0}, b : {1}", a, b);
+        }
+    }
+}
