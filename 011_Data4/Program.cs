28b840d3f183379237846b08a8584e275f8997fb
jihwan <jihwan1535@nate.com>
2023-08-03 15:05:53 +0900

    Base Conversion

---
+++ 011_Data4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _011_Data4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0b11111111;
+            int b = 0xFF;
+            int c = 255;
+
+            Console.WriteLine("2진수 : " + a);
+            Console.WriteLine("16진수 : " + b);
+            Console.WriteLine("10진수 : " + c);
+        }
+    }
+}
